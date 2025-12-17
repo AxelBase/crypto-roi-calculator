beae684cc53b61795a18cafe9c93c95881a42430
Sahil Ramjeeth <22046468@dut4life.ac.za>
2025-12-17 12:38:03 +0200

    GSC 1

---
+++ docs/_app/immutable/chunks/BWabMQg9.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1dkk595?.base??"/crypto-roi-calculator",a=globalThis.__sveltekit_1dkk595?.assets??s??"";export{a,s as b};
--- docs/_app/immutable/chunks/CRhzp5od.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1kwbu7r?.base??"/crypto-roi-calculator",a=globalThis.__sveltekit_1kwbu7r?.assets??s??"";export{a,s as b};
